c97773d188b123e9b6ed44df7e3c5168e457d3ca
Eric Echemane <eechemane29@gmail.com>
2022-09-06 20:40:34 +0800

    server initialized

---
+++ socket_server/index.js
@@ -0,0 +1,28 @@
+var PORT = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.Server(app);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+});
+
+server.listen(PORT, function () {
+    console.log('listening on *:' + PORT);
+}
+);
+
+var io = require('socket.io')(server);
+
+io.on('connect', function (socket) {
+
+    console.log('a user connected');
+
+    socket.on('message', function (data) {
+        console.log(data);
+    });
+
+});
+
+
