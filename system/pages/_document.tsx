a1b47db601e7404fcf6a44a5f5200e6b6913d356
Eric Echemane <eechemane29@gmail.com>
2022-09-07 08:55:39 +0800

    mui installed

---
+++ system/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
--- system/pages/index.tsx
+++ system/pages/index.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@mui/material";
+import Head from "next/head";
+
 export default function Home() {
   return <>
+    <Head>
+      <title>Security Support System Using RFID</title>
+    </Head>
     <h1> Hello rfid system </h1>
+    <Button variant="contained"> Save </Button>
   </>;
 }
