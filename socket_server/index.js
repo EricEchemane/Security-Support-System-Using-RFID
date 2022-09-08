0b3419e5dd3db3becca9fc09c416b22cb204143d
Eric Echemane <eechemane29@gmail.com>
2022-09-09 07:48:24 +0800

    new socket server

--- socket_server/index.js
+++ socket_server/index.js
@@ -1,30 +1,25 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
 const PORT = process.env.PORT || 4000;
-const express = require('express');
+
 const app = express();
-const http = require('http');
-const server = http.Server(app);
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, { cors: { origin: '*' } });
 
-server.listen(PORT, function () {
-    console.log(`🚀 App listening on port ${PORT}`);
-});
-
-const io = require('socket.io')(server, {
-    cors: {
-        origin: '*',
-    }
-});
-
-io.on('connection', function (socket) {
-    console.log('Client connected with id: ' + socket.id);
-
-    socket.on('time:in', uid => {
-        const timeIn = new Date();
-        console.log(`User ${uid} time in: ${timeIn}`);
-
-        socket.emit('time:in', { uid, timeIn });
-    });
+io.on('connection', socket => {
+    console.log('New Connection from', socket.id);
 
     socket.on('disconnect', () => {
-        console.log('Client disconnected with id: ' + socket.id);
+        console.log('Disconnected');
     });
+
+    socket.on('time:in', data => {
+        console.log(data);
+    });
+});
+
+httpServer.listen(PORT, () => {
+    console.log('Running on : ', httpServer.address());
 });
