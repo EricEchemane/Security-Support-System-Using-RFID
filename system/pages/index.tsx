263de08333e3aa932438ebdced962d82103cdea9
Eric Echemane <eechemane29@gmail.com>
2022-09-11 13:33:18 +0800

    new components folder

--- system/pages/index.tsx
+++ system/pages/index.tsx
@@ -1,6 +1,8 @@
 import useSocketConnection, { timeInData } from "@app/hooks/useSocketConnection";
+import { Button } from "@mui/material";
 import { useState } from "react";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   const [timeInData, setTimeInData] = useState<timeInData>();
@@ -11,5 +13,8 @@ export default function Home() {
   return <>
     <Head> <title> Security Support System Using RFID </title> </Head>
     {JSON.stringify(timeInData, null, 4)}
+    <Link passHref href={'/student/new'}>
+      <Button variant="contained"> add new student </Button>
+    </Link>
   </>;
 }
---
+++ system/pages/student/new.tsx
@@ -0,0 +1,8 @@
+import Head from "next/head";
+
+export default function NewStudent() {
+
+    return <>
+        <Head> <title> New Student </title> </Head>
+    </>;
+}
