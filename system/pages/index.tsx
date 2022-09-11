c567f894989d0ca0f53900756398b33ef97f5541
Eric Echemane <eechemane29@gmail.com>
2022-09-11 11:16:11 +0800

    added socket.off in useSocketConnection hook

--- system/pages/index.tsx
+++ system/pages/index.tsx
@@ -1,17 +1,19 @@
 import useSocketConnection, { timeInData } from "@app/hooks/useSocketConnection";
 import Head from "next/head";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
 export default function Home() {
-
+  const [timeInData, settimeInData] = useState<timeInData>();
   const onTimeIn = useCallback((data: timeInData) => {
-    console.log(data);
+    settimeInData(data);
   }, []);
 
-  useSocketConnection(onTimeIn);
+  const { socket } = useSocketConnection(onTimeIn);
 
 
   return <>
     <Head> <title> Security Support System Using RFID </title> </Head>
+    <h5> {socket?.connected ? 'connected' : 'disconnected'} </h5>
+    {JSON.stringify(timeInData, null, 4)}
   </>;
 }
