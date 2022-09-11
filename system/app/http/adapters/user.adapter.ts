219d17fd3c128a2288a978641be83ffabb1c1e35
Eric Echemane <eechemane29@gmail.com>
2022-09-11 14:44:01 +0800

    student adapter

--- system/app/http/adapters/user.adapter.ts
+++ system/app/http/adapters/user.adapter.ts
@@ -1,2 +1,11 @@
 import HttpAdapter from "../base-adapter";
 import useHttpAdapter from "../useHttpAdapter";
+
+const StudentAdapter = Object.freeze({
+    FindByRfid: () => useHttpAdapter<
+        null,
+        { rfid: string; }
+    >(new HttpAdapter("/api/student/:rfid", 'GET')),
+});
+
+export default StudentAdapter;
