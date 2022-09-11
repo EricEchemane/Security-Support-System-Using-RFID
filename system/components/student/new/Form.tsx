51dcf6eedb30a648559adde1c8aa034ae6f8a0d8
Eric Echemane <eechemane29@gmail.com>
2022-09-11 17:02:30 +0800

    fixed responsive stack

--- system/components/student/new/Form.tsx
+++ system/components/student/new/Form.tsx
@@ -9,29 +9,40 @@ export default function Form({ values, handleChange, connected }: {
     return <>
         <Container style={{ marginTop: '2rem' }}>
             <SocketConnectionStatus connected={connected} />
-            <Typography variant="h3" mt={2}> New Student </Typography>
+            <Typography variant="h3" mt={2}> New Student • {values.rfid} </Typography>
 
             <Stack style={{ marginTop: '2rem' }} spacing={2}>
-                <Stack direction={"row"} spacing={2}>
-                    <TextField
-                        fullWidth
-                        name="rfid"
-                        label="RFID"
-                        InputProps={{ readOnly: true }}
-                        defaultValue={values.rfid}
-                        variant="outlined" />
-                    <TextField
-                        fullWidth
-                        name="email"
-                        label="Email"
-                        value={values.email}
-                        onChange={handleChange}
-                        variant="outlined" />
+                <Typography variant="h5"> Basic info </Typography>
+                <Stack
+                    direction={{ xs: 'column', sm: 'row' }}
+                    spacing={2}>
                     <TextField
                         fullWidth
                         name="firstName"
                         label="First name"
                         value={values.firstName}
+                        onChange={handleChange}
+                        variant="outlined" />
+                    <TextField
+                        fullWidth
+                        name="middleName"
+                        label="Middle name"
+                        value={values.middleName}
+                        onChange={handleChange}
+                        variant="outlined" />
+                    <TextField
+                        fullWidth
+                        name="lastName"
+                        label="Last name"
+                        value={values.lastName}
+                        onChange={handleChange}
+                        variant="outlined" />
+                    <TextField
+                        fullWidth
+                        name="nameExtension"
+                        label="Name extension"
+                        helperText="eg: Jr."
+                        value={values.nameExtension}
                         onChange={handleChange}
                         variant="outlined" />
                 </Stack>
