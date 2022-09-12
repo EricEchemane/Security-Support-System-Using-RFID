6456c996ec9f326f2f5814b3e44664977c90ec85
Eric Echemane <eechemane29@gmail.com>
2022-09-12 21:50:40 +0800

    db-connection

---
+++ server/db/db-connection.ts
@@ -0,0 +1,25 @@
+import Staff from "@entities/staff.entity";
+import Student from "@entities/student.model";
+import mongoose from "mongoose";
+
+type DB = typeof mongoose & {
+    models: {
+        Student: typeof Student;
+        Staff: typeof Staff;
+    };
+};
+
+let db: DB;
+
+export default async function getDbConnection(): Promise<DB> {
+    if (db) return db;
+
+    const uri = process.env.MONGODB_URI;
+    if (!uri) throw new Error("MONGODB_URI is not defined");
+
+    db = (await mongoose.connect(uri)) as DB;
+    db.models.Student = Student;
+    db.models.Staff = Staff;
+
+    return db;
+}
