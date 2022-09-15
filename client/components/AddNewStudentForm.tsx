ae5a756a5de8aa343d832b090fdcb592b124b3c4
Eric Echemane <eechemane29@gmail.com>
2022-09-15 13:22:46 +0800

    datepicker test

--- client/components/AddNewStudentForm.tsx
+++ client/components/AddNewStudentForm.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import dayjs, { Dayjs } from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { TextField } from '@mui/material';
 
 export default function AddNewStudentForm(props: {
     uid: string;
 }) {
-    return (
-        <div>AddNewStudentForm</div>
+    const [value, setValue] = React.useState<Dayjs | null>(
+        dayjs('2014-08-18T21:11:54'),
     );
+    const handleChange = (newValue: Dayjs | null) => {
+        setValue(newValue);
+    };
+    return <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <MobileDatePicker
+            label="Date mobile"
+            inputFormat="MM/DD/YYYY"
+            value={value}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+        />
+    </LocalizationProvider>;
 }
