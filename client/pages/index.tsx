48bba28c5a4389a7e9f1c7bf2953f5a0ef9c13a0
Eric Echemane <eechemane29@gmail.com>
2022-09-25 09:28:46 +0800

    persist logged in to session

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,22 @@
 import HomePage from 'components/Home';
 import Login from 'components/Login';
 import Head from 'next/head';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const sessionKey = "sbca-admin";
   const [loggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+    const session = sessionStorage.getItem(sessionKey);
+    if (session) setLoggedIn(true);
+  }, []);
+  const login = () => {
+    sessionStorage.setItem(sessionKey, sessionKey);
+    setLoggedIn(true);
+  };
+
   return <>
     <Head> <title> Security Support System Using RFID </title> </Head>
-    {loggedIn ? <HomePage /> : <Login onLoggedInSuccess={() => setLoggedIn(true)} />}
+    {loggedIn ? <HomePage /> : <Login onLoggedInSuccess={login} />}
   </>;
 }
