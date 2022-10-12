ec5de796a8c6e64767587ff9a65c060a2b19ddef
Eric Echemane <eechemane29@gmail.com>
2022-10-12 22:42:34 +0800

    extracted footer to its own component

---
+++ client/components/Footer.tsx
@@ -0,0 +1,37 @@
+import { Email, Phone, Language, LocationOn } from '@mui/icons-material';
+import { Stack, Typography } from '@mui/material';
+
+export default function Footer() {
+    return (
+        <Stack
+            mt={7}
+            direction="row"
+            justifyContent="center"
+            spacing={8}>
+            <Stack direction="row" justifyContent="center" alignContent="center" spacing={1}>
+                <Phone />
+                <Typography variant='h5'>
+                    288422139
+                </Typography>
+            </Stack>
+            <Stack direction="row" justifyContent="center" alignContent="center" spacing={1}>
+                <Email />
+                <Typography variant='h5'>
+                    admin@sbca.edu.ph
+                </Typography>
+            </Stack>
+            <Stack direction="row" justifyContent="center" alignContent="center" spacing={1}>
+                <Language />
+                <Typography variant='h5'>
+                    www.sbca.edu.ph
+                </Typography>
+            </Stack>
+            <Stack direction="row" justifyContent="center" alignContent="center" spacing={1}>
+                <LocationOn />
+                <Typography variant='h5'>
+                    Km. 23. 6 East Service Road, Muntinlupa City
+                </Typography>
+            </Stack>
+        </Stack>
+    );
+}
--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
-import { Email, Phone, Language, LocationOn } from '@mui/icons-material';
 import { Button, Container, Stack, Typography } from '@mui/material';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react';
 import sbcaLogo from "../public/assets/logo.png";
+import Footer from './Footer';
 
 export default function HomePage() {
     const router = useRouter();
@@ -77,36 +77,7 @@ export default function HomePage() {
                 </Stack>
             </Container>
 
-            <Stack
-                mt={7}
-                direction="row"
-                justifyContent="center"
-                spacing={8}>
-                <Stack direction="row" justifyContent="center" alignContent="center" spacing={1}>
-                    <Phone />
-                    <Typography variant='h5'>
-                        288422139
-                    </Typography>
-                </Stack>
-                <Stack direction="row" justifyContent="center" alignContent="center" spacing={1}>
-                    <Email />
-                    <Typography variant='h5'>
-                        admin@sbca.edu.ph
-                    </Typography>
-                </Stack>
-                <Stack direction="row" justifyContent="center" alignContent="center" spacing={1}>
-                    <Language />
-                    <Typography variant='h5'>
-                        www.sbca.edu.ph
-                    </Typography>
-                </Stack>
-                <Stack direction="row" justifyContent="center" alignContent="center" spacing={1}>
-                    <LocationOn />
-                    <Typography variant='h5'>
-                        Km. 23. 6 East Service Road, Muntinlupa City
-                    </Typography>
-                </Stack>
-            </Stack>
+            <Footer />
         </div>
     </>;
 }
