8ef53b76de30170b9df8189c848919a4f63d6dc0
Eric Echemane <eechemane29@gmail.com>
2023-01-05 17:03:32 +0800

    new server

--- client/lib/socketConfig.ts
+++ client/lib/socketConfig.ts
@@ -1,8 +1,11 @@
 const socketConfig = Object.freeze({
-    url: process.env.NODE_ENV === 'production' ? 'https://sbca-server.herokuapp.com' : 'http://localhost:4000',
-    options: {
-        transports: ['websocket'],
-    },
+	url:
+		process.env.NODE_ENV === 'production'
+			? 'https://sbca-server-production.up.railway.app'
+			: 'http://localhost:4000',
+	options: {
+		transports: ['websocket'],
+	},
 });
 
 export default socketConfig;
